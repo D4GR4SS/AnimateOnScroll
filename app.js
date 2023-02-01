b589e703aca2297dcfbfc05e40f3f744ad1359b2
Davide Grassitelli <dagrassitelli@gmail.com>
2023-02-01 15:42:07 +0100

    second try way better

--- app.js
+++ app.js
@@ -12,3 +12,25 @@
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
  */
+
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkBoxes);
+
+function checkBoxes() {
+  console.log((window.innerHeight / 5) * 4);
+
+  const triggerBottom = (window.innerHeight / 5) * 4;
+
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      box.classList.add('show');
+    } else {
+      box.classList.remove('show');
+    }
+  });
+}
+
+checkBoxes();
